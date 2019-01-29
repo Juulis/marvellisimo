d924fac87ecac40159077188c613b75ebd0830ac
schonis86 <calleschonberg@gmail.com>
2019-01-29 14:10:26 +0100

    adds characte class

---
+++ app/src/main/java/malidaca/marvellisimo/adapters/CharacterListAdapter.kt
@@ -0,0 +1,31 @@
+package malidaca.marvellisimo.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import kotlinx.android.synthetic.main.character_list_card.view.*
+import malidaca.marvellisimo.R
+import malidaca.marvellisimo.models.Character
+
+class CharacterListAdapter(private val characters: Array<Character>, private val context: Context): RecyclerView.Adapter<ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.character_list_card, p0, false))    }
+
+    override fun getItemCount(): Int {
+        return characters.size    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        p0.name.text = characters[p1].name
+    }
+
+
+}
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        val name = view.name!!
+        val img = view.img!!
+        val imgview = view.imageView!!
+    }
---
+++ app/src/main/java/malidaca/marvellisimo/models/Character.kt
@@ -0,0 +1,10 @@
+package malidaca.marvellisimo.models
+
+class Character(
+        val id: Int,
+        val name: String,
+        val description: String,
+        val resourceURI: String,
+        val thumbnail: Picture,
+        val series: ItemList
+)
