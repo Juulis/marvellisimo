7b226b7e40a36542b58c3f6e8a071896f1a13bb3
Juulis <danny.p@live.se>
2019-01-30 11:10:48 +0100

    service is now global

--- app/src/main/java/malidaca/marvellisimo/activities/MainActivity.kt
+++ app/src/main/java/malidaca/marvellisimo/activities/MainActivity.kt
@@ -1,10 +1,10 @@
-package malidaca.marvellisimo
+package malidaca.marvellisimo.activities
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import malidaca.marvellisimo.activities.SeriesActivity
+import malidaca.marvellisimo.R
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/malidaca/marvellisimo/rest/MarvelService.kt
+++ app/src/main/java/malidaca/marvellisimo/rest/MarvelService.kt
@@ -1,14 +1,14 @@
 package malidaca.marvellisimo.rest
 
+import io.reactivex.Single
 import malidaca.marvellisimo.models.ApiResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MarvelService{
 
-    @GET("/v1/public/series")
+    @GET("series")
     fun getSeries(@Query("ts") ts: String,
                   @Query("apikey") apikey: String,
-                  @Query("hash") hash: String): Call<ApiResponse>
+                  @Query("hash") hash: String): Single<ApiResponse>
 }
