4764a70f3b7315f144dcb050bec9e775cde43f30
Max Barnell <max.barnell@gmail.com>
2019-02-05 12:54:51 +0100

    Added SnackbarManager

---
+++ app/src/main/java/malidaca/marvellisimo/utilities/SnackbarManager.kt
@@ -0,0 +1,24 @@
+package malidaca.marvellisimo.utilities
+
+import android.graphics.Color
+import android.support.design.widget.Snackbar
+import android.view.Gravity
+import android.view.View
+import android.view.View.TEXT_ALIGNMENT_CENTER
+import android.widget.FrameLayout
+import android.widget.TextView
+
+class SnackbarManager {
+    fun createSnackbar(view: View, text: String, color: Int, gravity: Int = Gravity.TOP) {
+        val mSnackBar = Snackbar.make(view, text, Snackbar.LENGTH_LONG)
+        val sView = mSnackBar.view
+        val params = sView.layoutParams as FrameLayout.LayoutParams
+        params.gravity = gravity
+        sView.layoutParams = params
+        sView.setBackgroundColor(color)
+        val mainTextView = sView.findViewById(android.support.design.R.id.snackbar_text) as TextView
+        mainTextView.setTextColor(Color.WHITE)
+        mainTextView.textAlignment = TEXT_ALIGNMENT_CENTER
+        mSnackBar.show()
+    }
+}
