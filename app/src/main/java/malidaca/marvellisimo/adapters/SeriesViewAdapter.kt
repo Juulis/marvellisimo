4674e74401e47f419f8c1a1f091b92ead820d19f
LidiaT <tothlidia80@gmail.com>
2019-02-05 21:05:01 +0100

    added images to lists

--- app/src/main/java/malidaca/marvellisimo/adapters/SeriesViewAdapter.kt
+++ app/src/main/java/malidaca/marvellisimo/adapters/SeriesViewAdapter.kt
@@ -1,14 +1,14 @@
 package malidaca.marvellisimo.adapters
 import android.content.Context
 import android.content.Intent
-import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.character_list_card.view.*
 import malidaca.marvellisimo.R
-import malidaca.marvellisimo.activities.SeriesActivity
 import malidaca.marvellisimo.activities.SeriesDetailsActivity
 import malidaca.marvellisimo.models.Series
 
@@ -16,14 +16,17 @@ class SeriesViewAdapter(private var series: List<Series>, private val context: C
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.series_view, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.character_list_card, parent, false)
         view.setOnClickListener(this)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.itemView.tag = series[position]
-        holder.myTextView.text = series[position].title
+        holder.title.text = series[position].title
+        var url = "${series[position].thumbnail.path}/landscape_large.${series[position].thumbnail.extension}"
+        url = url.replace("http", "https")
+        Picasso.get().load(url).into(holder.img)
     }
 
     override fun getItemCount() = series.size
@@ -47,6 +50,7 @@ class SeriesViewAdapter(private var series: List<Series>, private val context: C
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        val myTextView = itemView.findViewById(R.id.title) as TextView
+        val title = itemView.name!!
+        val img = itemView.img!!
     }
 }
