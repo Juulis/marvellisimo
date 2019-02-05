a68f6febd920daf96c23b550dc5da7a4dd82ffd4
LidiaT <tothlidia80@gmail.com>
2019-02-05 10:42:29 +0100

    show details of a series from character

--- app/src/main/java/malidaca/marvellisimo/adapters/SeriesListAdapter.kt
+++ app/src/main/java/malidaca/marvellisimo/adapters/SeriesListAdapter.kt
@@ -1,6 +1,7 @@
 package malidaca.marvellisimo.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.series_list_view_card.view.*
 import malidaca.marvellisimo.R
 import malidaca.marvellisimo.activities.ItemClickListener
+import malidaca.marvellisimo.activities.SeriesDetailsActivity
 import malidaca.marvellisimo.models.Series
 
 class SeriesListAdapter(private var series: List<Series>, private val context: Context): RecyclerView.Adapter<ViewHolderTwo>() {
@@ -33,11 +35,10 @@ class SeriesListAdapter(private var series: List<Series>, private val context: C
 
         holder.setOnItemClickListener(object : ItemClickListener {
             override fun onCustomClickListener(view: View, pos: Int) {
-              /*  val intent = Intent(context, CharacterActivity::class.java)
-                intent.putExtra("itemId",series[position].id )
-                context.startActivity(intent)*/
+                val intent = Intent(context, SeriesDetailsActivity::class.java)
+                intent.putExtra("id",series[position].id )
+                context.startActivity(intent)
             }
-
         })
     }
 
