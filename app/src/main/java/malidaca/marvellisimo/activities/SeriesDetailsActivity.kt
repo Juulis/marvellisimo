5fb728f53ae98f5eb985dddb9069d148b9ac324a
LidiaT <tothlidia80@gmail.com>
2019-02-06 20:33:22 +0100

    fixed scroll in series details

--- app/src/main/java/malidaca/marvellisimo/activities/SeriesDetailsActivity.kt
+++ app/src/main/java/malidaca/marvellisimo/activities/SeriesDetailsActivity.kt
@@ -1,6 +1,5 @@
 package malidaca.marvellisimo.activities
 
-import android.content.Intent
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
