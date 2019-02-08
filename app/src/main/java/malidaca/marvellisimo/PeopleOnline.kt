e142aa061f26a5bc613dd7fca79ce139942576da
Max Barnell <max.barnell@gmail.com>
2019-02-08 11:57:21 +0100

    Can print out names in fragment

--- app/src/main/java/malidaca/marvellisimo/PeopleOnline.kt
+++ app/src/main/java/malidaca/marvellisimo/PeopleOnline.kt
@@ -3,9 +3,15 @@ package malidaca.marvellisimo
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.people_online_fragment.view.*
+import malidaca.marvellisimo.adapters.PeopleListAdapter
+
+
 
 
 class PeopleOnline : Fragment() {
@@ -14,17 +20,31 @@ class PeopleOnline : Fragment() {
         fun newInstance() = PeopleOnline()
     }
 
+    val names: ArrayList<String> = ArrayList()
+
     private lateinit var viewModel: PeopleOnlineViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.people_online_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.people_online_fragment, container, false)
+        addNames()
+        val peopleOnline = rootView.people_online_list
+        peopleOnline.layoutManager = LinearLayoutManager(activity)
+        val adapter = PeopleListAdapter(names)
+        peopleOnline.adapter = adapter
+        peopleOnline.itemAnimator = DefaultItemAnimator()
+        return rootView
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
+    fun addNames() {
+        names.add("Stefan")
+        names.add("Karin")
+    }
+
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(PeopleOnlineViewModel::class.java)
         // TODO: Use the ViewModel
-    }
+    }*/
 
 }
--- app/src/main/java/malidaca/marvellisimo/adapters/PeopleListAdapter.kt
+++ app/src/main/java/malidaca/marvellisimo/adapters/PeopleListAdapter.kt
@@ -1,18 +1,68 @@
 package malidaca.marvellisimo.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.people_online_card.view.*
+import malidaca.marvellisimo.R
+import malidaca.marvellisimo.activities.ItemClickListener
 
-class PeopleListAdapter : RecyclerView.Adapter<ViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+class PeopleListAdapter(private var names: List<String>) : RecyclerView.Adapter<PeopleListAdapter.ViewHolder>() {//}, View.OnClickListener {
+    //override fun onClick(v: View?) {
+    //    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    //}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PeopleListAdapter.ViewHolder {
+        //return ViewHolder(LayoutInflater.from(context).inflate(R.layout.people_online_card, viewGroup, false))
+
+        val itemLayoutView = LayoutInflater.from(parent.context).inflate(R.layout.people_online_card, parent,false)
+
+        //itemLayoutView.setOnClickListener(this)
+        // create ViewHolder
+
+        return ViewHolder(itemLayoutView)
     }
+
+    fun addItems(list: List<String>){
+        names = list
+        notifyDataSetChanged()
+    }
+
+    /*override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setOnItemClickListener(object : ItemClickListener {
+            override fun onCustomClickListener(view: View, pos: Int) {
+            }
+        })
+    }*/
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.text = names[position]
+    }
+
+
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return names.size
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {//}, View.OnClickListener {
+        val name = view.name
+        private var itemClickListener: ItemClickListener? = null
+
+        init {
+            //name.setOnClickListener(this)
+        }
+
+        fun setOnItemClickListener(itemClickListener: ItemClickListener) {
+            this.itemClickListener = itemClickListener
+        }
+
+        //override fun onClick(v: View?) {
+        //    this.itemClickListener!!.onCustomClickListener(v!!, adapterPosition)
+        //}
     }
 }
+
+
